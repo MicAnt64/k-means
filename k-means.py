f5f9349c3d88034ac9e8b6ac563973153b2d7252
Michael Antia <michael_antia@hotmail.com>
2020-05-04 16:09:42 -0400

    Add label color coding func

--- k-means.py
+++ k-means.py
@@ -32,43 +32,38 @@ z = np.hstack((z1,z2,z3))
 
 X = np.vstack((x,y,z)).T
 np.random.shuffle(X)
-labels = ['red' if c == 1 else 'green' if c == 2 else 'blue' for c in X[:,2]] 
+labels_init = ['red' if c == 1 else 'green' if c == 2 else 'blue' for c in X[:,2]] 
 X_data = X[:,:2]
+
 
 def plot_and_save_fig(data, title, labels):
     plt.scatter(data[:,0], data[:,1], color=labels)
     plt.savefig(title)
     plt.show()
 
-
-# Select k
-k = 3
+def labelColorCoding(data):
+    return ['red' if c == 1 else 'green' if c == 2 else 'blue' for c in data] 
 
 # Initialize K random points (indices) for cluster centers
 def select_k_random_points(data, k):
     idx = np.random.choice(data.shape[0], size=k)
     return data[idx,:]
 
-m_k = select_k_random_points(X_data, k)
-
-
 # Distance function
 def euclideanDistance(data, testPoint):
     return np.sqrt(np.sum((data - testPoint)**2, axis=1))
 
 # def compute nxk distances
-
 def k_distances(data, m_k, func):
     distances = np.zeros((data.shape[0], m_k.shape[0]))
     for i in range(m_k.shape[0]):
         distances[:,i] = func(data, m_k[i,:])
     return distances
 
-distances = k_distances(X_data, m_k, euclideanDistance) 
-
 # Find smallest distance and return index, expected output n by k 
 # This is the cluster assignment
-cluster_assignment = np.argmin(distances, axis=1)
+def assignToCluster(distances):
+    return np.argmin(distances, axis=1)
 
 # Compute Cost J
 def costFunction(data, distances, cluster_assignments):
@@ -84,4 +79,17 @@ def converge(distanceOld, distanceNew, epsilon=0.0001):
     else:
         return False
 
-    
+
+# Select k
+k = 3
+m_k = select_k_random_points(X_data, k)
+distances = k_distances(X_data, m_k, euclideanDistance) 
+cluster_assignment = assignToCluster(distances)
+
+
+
+
+
+
+
+
